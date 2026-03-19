f045dad185cebf3cc2765ab195bb5e861187cee2
liamvnastoria <liam.dispa@skygenesisenterprise.com>
2026-03-19 05:28:00 +0100

    refactor: restructure server package layout Move server/src/controllers/ into a flat structure with dedicated subdirectories: api/, common/, install/, private/, utils/, web/

---
+++ server/web/org/projects_test.go
@@ -0,0 +1,28 @@
+// Copyright 2022 The Gitea Authors. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+package org_test
+
+import (
+	"testing"
+
+	"github.com/skygenesisenterprise/giteria/models/unittest"
+	"github.com/skygenesisenterprise/giteria/server/web/org"
+	"github.com/skygenesisenterprise/giteria/services/contexttest"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCheckProjectColumnChangePermissions(t *testing.T) {
+	unittest.PrepareTestEnv(t)
+	ctx, _ := contexttest.MockContext(t, "user2/-/projects/4/4")
+	contexttest.LoadUser(t, ctx, 2)
+	ctx.ContextUser = ctx.Doer // user2
+	ctx.SetPathParam("id", "4")
+	ctx.SetPathParam("columnID", "4")
+
+	project, column := org.CheckProjectColumnChangePermissions(ctx)
+	assert.NotNil(t, project)
+	assert.NotNil(t, column)
+	assert.False(t, ctx.Written())
+}
